c747b200a5ccd319e4865d26b12db126f57c046c
Taoufiq El Moutaouakil <rztaoufiq@gmail.com>
2024-10-07 16:16:50 -0700

    my first commit in weather wise application

---
+++ WeatherWise/WeatherWise/WeatherWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWiseApp.swift
+//  WeatherWise
+//
+//  Created by Taooufiq El moutaoouakil on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
