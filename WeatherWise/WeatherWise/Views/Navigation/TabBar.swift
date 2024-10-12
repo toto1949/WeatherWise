64d614708ceb13dd57aa5f08332df1a7b796d0cd
Taoufiq El Moutaouakil <rztaoufiq@gmail.com>
2024-10-12 02:22:12 -0700

    Add tab bar and navigation bar

--- WeatherWise/WeatherWise/Views/Navigation/TabBar.swift
+++ WeatherWise/WeatherWise/Views/Navigation/TabBar.swift
@@ -2,38 +2,40 @@
 //  TabBar.swift
 //  WeatherWise
 //
-//  Created by Taooufiq El moutaoouakil on 10/7/24.
+//  Created by Taoufiq El Moutaouakil on 10/7/24.
 //
+
+
 
 import SwiftUI
 
 struct TabBar: View {
-    var action : () -> Void
+    var action: () -> Void
+    @ObservedObject var viewModel: WeatherViewModel
+
     var body: some View {
-        ZStack{
-            //MARK: Arc Shape
+        ZStack {
             Arc()
                 .fill(Color.tabBarBackground)
                 .frame(height: 88)
-                .overlay{
+                .overlay {
                     Arc()
-                        .stroke(Color.tabBarBorder,lineWidth:  0.5)
+                        .stroke(Color.tabBarBorder, lineWidth: 0.5)
                 }
-            // MARK: Tab Items
-            HStack{
-           // MARK: Expand Button
-                Button{
+            
+            HStack {
+                Button {
                     action()
-                }
-                label: {
+                } label: {
                     Image(systemName: "mappin.and.ellipse")
-                        .frame(width: 44,height: 44)
+                        .frame(width: 44, height: 44)
                 }
+                
                 Spacer()
-                // MARK: Navigation Button
-                NavigationLink{
-                    
-                }label: {
+
+                NavigationLink {
+                    WeatherView(viewModel: viewModel)
+                } label: {
                     Image(systemName: "list.star")
                         .frame(width: 44, height: 44)
                 }
@@ -41,14 +43,8 @@ struct TabBar: View {
             .font(.title2)
             .foregroundColor(.white)
             .padding(EdgeInsets(top: 20, leading: 32, bottom: 24, trailing: 32))
-
         }
-        .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .bottom)
+        .frame(maxHeight: .infinity, alignment: .bottom)
         .ignoresSafeArea()
     }
-}
-
-#Preview {
-    TabBar(action: {})
-        .preferredColorScheme(.dark)
 }
